d5d27783ea37c86431ca0d4b4b0681706344f495
Luis Felipe de Souza Bond <69223872+lfbond@users.noreply.github.com>
2022-05-03 15:21:22 -0300

    Add files via upload

---
+++ js/slider.js
@@ -0,0 +1,20 @@
+$('.container-slider').slick({
+    dots:true,
+    arrows:false,
+    infinite:true,
+    speed:1000,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+    autoplay:true,
+    autoplaySpeed: 3000,
+    pauseOnHover: false,
+    responsive: [
+        {
+            breakpoint: 768,
+            settings: {
+                slidesToScroll: 1,
+                slidesToShow: 1
+            }
+        }
+    ]
+});
